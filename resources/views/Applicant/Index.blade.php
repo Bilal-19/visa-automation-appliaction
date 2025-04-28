34207f1191d7a1d450eefdce17a0c9099e7edbd1
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-04-28 17:56:24 +0500

    Updated home page.

--- resources/views/Applicant/Index.blade.php
+++ resources/views/Applicant/Index.blade.php
@@ -1,5 +1,14 @@
-@extends("ApplicantLayout.main")
+@extends('ApplicantLayout.main')
 
-@section("main-section")
-
+@section('main-section')
+    <div class="w-full grid grid-col-1 md:grid-cols-2 gap-12 p-10 md:p-20">
+        <div class="border-t-1 border-x-1 border-b-4 p-5 rounded-md bg-gray-200 shadow-md">
+            <h1>Step 01: Choose Your Visa Type</h1>
+            <p>Select the visa type</p>
+        </div>
+        <div class="border-t-1 border-x-1 border-b-4 p-5 rounded-md bg-gray-200 shadow-md">
+            <h1>Step 01: Choose Your Visa Type</h1>
+            <p>Select the visa type</p>
+        </div>
+    </div>
 @endsection
