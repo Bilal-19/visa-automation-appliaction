5917ef1d5c486590bcb6e8cd9915fdecbb717841
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-04-30 17:34:37 +0500

    Updated admin dashboard.

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -1,7 +1,33 @@
 @extends('AdminLayout.main')
 
 @section('section')
-    <div class="w-full">
+    <main class="flex-1 p-6 overflow-x-auto">
+        <div class="w-full mt-5 bg-white rounded p-6">
+            <h2 class="text-xl font-semibold mb-5">Admin Dashboard</h2>
 
+            <div class="grid grid-cols-3 gap-10">
+                <div class="bg-gray-200 rounded-md p-5 text-center border-x-1 border-b-4 border-gray-900">
+                    <h3 class="font-medium text-xl">Total Visa Applicants</h3>
+                    <p class="md:text-lg">
+                        <i class="fa-solid fa-users-between-lines text-blue-700"></i> 100
+                    </p>
+                </div>
+
+                <div class="bg-gray-200 rounded-md p-5 text-center border-x-1 border-b-4 border-gray-900">
+                    <h3 class="font-medium text-xl">Approved Applications</h3>
+                    <p class="md:text-lg">
+                        <i class="fa-solid fa-users-line text-green-700"></i> 100
+                    </p>
+                </div>
+
+                <div class="bg-gray-200 rounded-md p-5 text-center border-x-1 border-b-4 border-gray-900">
+                    <h3 class="font-medium text-xl">Reject Applications</h3>
+                    <p class="md:text-lg">
+                        <i class="fa-solid fa-users-slash text-red-700"></i> 100
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
     </div>
 @endsection
